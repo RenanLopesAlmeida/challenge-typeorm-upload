0033eaeceb95c4479064f5969ff5e33453f5180e
RenanLopesAlmeida <renan_lopes_almeida@hotmail.com>
2020-06-30 20:47:50 -0300

    :rocket: Challenge completed

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,94 @@
-import Transaction from '../models/Transaction';
+import { getCustomRepository, getRepository, In } from 'typeorm';
+import fs from 'fs';
+import csvParse from 'csv-parse';
 
+import TransactionRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface CSVTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+/**
+ * tenta ler o arquivo
+ * no parser, o csvParser vai ler a partir da 2ª linha, ou seja, não vai ler o header
+ */
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      delimiter: ',',
+      from_line: 2,
+    });
+
+    // o pipe vai ler as linhas do csv assim que elas vão ficando disponiveis
+    const parseCSV = contactsReadStream.pipe(parsers);
+
+    const transactions: CSVTransactionDTO[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    // all the categories that are in category table
+    const existentCategories = await categoryRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existentCategoriesTitle = existentCategories.map(
+      (category: Category) => category.title,
+    );
+
+    // categories I dont have in database | filter using indexOf to remove duplicates registers
+    const addCategoryTitles = categories
+      .filter(category => !existentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoryRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoryRepository.save(newCategories);
+
+    const finalCategories = [...newCategories, ...existentCategories];
+
+    const createdTransactions = transactionRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        value: transaction.value,
+        type: transaction.type,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionRepository.save(createdTransactions);
+    await fs.promises.unlink(filePath);
+
+    return createdTransactions;
   }
 }
 
